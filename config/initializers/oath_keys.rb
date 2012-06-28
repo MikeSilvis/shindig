e55004a6ca7692894894e591b2a7b0b8b1014f47
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-06-27 23:45:25 -0400

    fixed oath keys

--- config/initializers/oath_keys.rb
+++ config/initializers/oath_keys.rb
@@ -2,11 +2,10 @@ unless defined?(GOOGLE_KEY)
   TWITTER_KEY = "2yBLkeHyddvYDCkLNvZXQ"
   TWITTER_SECRET = "sbevPXWzNwyrrKOob4poYGlPFGocNYHk7U75QjFcm4"
 
-  if Rails.production?
+  if Rails.env.production?
   	GOOGLE_KEY = "15981128324-0552ddk1tqq42urulmllmjekts5fdr1k.apps.googleusercontent.com"
 	  GOOGLE_SECRET = "Fojfrb9Pqrhsyo0auEpd62LZ"
   else
-
 	  GOOGLE_KEY = "15981128324.apps.googleusercontent.com"
 	  GOOGLE_SECRET = "uUzh3l2PiMx8MbrLNgNZRl5E"
 	end
