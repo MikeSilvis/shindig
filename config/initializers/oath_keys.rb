dae58266bbf211dbccfd1805e031fbe308e3c301
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-06-28 08:13:44 -0400

    changing oath keys

--- config/initializers/oath_keys.rb
+++ config/initializers/oath_keys.rb
@@ -1,12 +1,10 @@
-unless defined?(GOOGLE_KEY) && defined?(TWITTER_KEY)
-  TWITTER_KEY = "2yBLkeHyddvYDCkLNvZXQ"
-  TWITTER_SECRET = "sbevPXWzNwyrrKOob4poYGlPFGocNYHk7U75QjFcm4"
+TWITTER_KEY = "2yBLkeHyddvYDCkLNvZXQ"
+TWITTER_SECRET = "sbevPXWzNwyrrKOob4poYGlPFGocNYHk7U75QjFcm4"
 
-  if Rails.env.production?
-  	GOOGLE_KEY = "15981128324-0552ddk1tqq42urulmllmjekts5fdr1k.apps.googleusercontent.com"
-	  GOOGLE_SECRET = "Fojfrb9Pqrhsyo0auEpd62LZ"
-  else
-	  GOOGLE_KEY = "15981128324.apps.googleusercontent.com"
-	  GOOGLE_SECRET = "uUzh3l2PiMx8MbrLNgNZRl5E"
-	end
+if Rails.env.production?
+	GOOGLE_KEY = "15981128324-0552ddk1tqq42urulmllmjekts5fdr1k.apps.googleusercontent.com"
+  GOOGLE_SECRET = "Fojfrb9Pqrhsyo0auEpd62LZ"
+else
+  GOOGLE_KEY = "15981128324.apps.googleusercontent.com"
+  GOOGLE_SECRET = "uUzh3l2PiMx8MbrLNgNZRl5E"
 end
