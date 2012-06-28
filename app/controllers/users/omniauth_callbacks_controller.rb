2cb41142fc34de41369d50b6541a2511fb7317fb
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-06-28 10:44:52 -0400

    changing service to service_provider

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,10 +1,10 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-  SERVICES.each do |service|
-    define_method "#{service}".to_sym do
-      # location = redirect_location(session[:redirect_url])
-      current_user ? create_auth(service) : verify_user_and_auth(service)
-      redirect_to root_url, notice: "#{service.titleize} Registration Successful."
+  SERVICES.each do |service_provider|
+    define_method service_provider do
+      location = redirect_location(session[:redirect_url])
+      current_user ? create_auth(service_provider) : verify_user_and_auth(service_provider)
+      redirect_to root_url, notice: "#{service_provider.titleize} Registration Successful."
     end
   end
 
@@ -14,13 +14,13 @@ private
     redirect_url || root_url
   end
 
-  def verify_user_and_auth(service)
-    sign_in(User.send("verify_#{service}".to_sym, data).user)
+  def verify_user_and_auth(service_provider)
+    sign_in(User.send("verify_#{service_provider}".to_sym, data).user)
   end
 
-  def create_auth(service)
-    current_user.update_attributes(User.send("#{service}_hash".to_sym, data["info"]))
-    current_user.authentications.send("find_or_create_#{service}".to_sym, data)
+  def create_auth(service_provider)
+    current_user.update_attributes(User.send("#{service_provider}_hash".to_sym, data["info"]))
+    current_user.authentications.send("find_or_create_#{service_provider}".to_sym, data)
   end
 
   def data
