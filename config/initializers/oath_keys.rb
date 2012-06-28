a859ae794011092918f7373a06fbf3b382a913c7
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-06-27 23:38:31 -0400

    changed google oath for server

--- config/initializers/oath_keys.rb
+++ config/initializers/oath_keys.rb
@@ -2,6 +2,12 @@ unless defined?(GOOGLE_KEY)
   TWITTER_KEY = "2yBLkeHyddvYDCkLNvZXQ"
   TWITTER_SECRET = "sbevPXWzNwyrrKOob4poYGlPFGocNYHk7U75QjFcm4"
 
-  GOOGLE_KEY = "15981128324.apps.googleusercontent.com"
-  GOOGLE_SECRET = "uUzh3l2PiMx8MbrLNgNZRl5E"
+  if Rails.production?
+  	GOOGLE_KEY = "15981128324-0552ddk1tqq42urulmllmjekts5fdr1k.apps.googleusercontent.com"
+	  GOOGLE_SECRET = "Fojfrb9Pqrhsyo0auEpd62LZ"
+  else
+
+	  GOOGLE_KEY = "15981128324.apps.googleusercontent.com"
+	  GOOGLE_SECRET = "uUzh3l2PiMx8MbrLNgNZRl5E"
+	end
 end
