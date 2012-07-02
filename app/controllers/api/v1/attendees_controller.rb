dee4e81a88a5b9c35b8990965c078615167b06a1
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-07-02 09:57:30 -0400

    caching is gone

--- app/controllers/api/v1/attendees_controller.rb
+++ app/controllers/api/v1/attendees_controller.rb
@@ -1,7 +1,5 @@
 class Api::V1::AttendeesController < ApplicationController
   before_filter :require_login, only: [:index, :show, :update, :current]
-  caches_page :index, :show
-  cache_sweeper :attendee_sweeper
 
   def index
     @attendees = Attendee.find_attendees_except_self(params[:event_id],
