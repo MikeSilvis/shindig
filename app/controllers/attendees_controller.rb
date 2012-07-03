ca819c49925025a059ed196187e8e0c9f46118c0
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-07-03 08:18:16 -0400

    removing the wor dude for melanie

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -8,7 +8,7 @@ class AttendeesController < ApplicationController
   def create_attendee
     event = Event.find_by_token(params[:event_id])
     event.join_event(current_user.id)
-    redirect_to event_path(event.id), notice: "Radical dude, this shindig is going to rock now that you're coming!"
+    redirect_to event_path(event.id), notice: "Radical, this shindig is going to rock now that you're coming!"
   end
 
 end
